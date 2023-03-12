320bc548fa1659a5444fe65c3d9609d0ce6473be
yjs <doosan9551@naver.com>
2023-03-12 21:04:13 +0900

    이코테_25_실패율_0310.py

--- 정렬/이코테_25_실패율_0310.py
+++ 정렬/이코테_25_실패율_0310.py
@@ -1,15 +1,15 @@
 def solution(N, stages):
     k=len(stages)
     s=[]
-    for i in range(1,N+1):
+    for i in range(1,N+1):    # 스테이지 설정
         c=0
-        for j in range(len(stages)):
+        for j in range(len(stages)):    # 그 스테이지의 실패한 인원 카운트
             if stages[j] == i:
                 c += 1
         if c == 0:
             s.append(0)
         else:
-            s.append(c/k)
+            s.append(c/k)       # s에 구한 확률 넣기
         k = k- c
 
     a=sorted(s,reverse=True)
